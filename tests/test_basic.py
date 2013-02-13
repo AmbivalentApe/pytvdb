4802460f6955355ec14ed3fbd45ecf4d6e70f794
charver <philipanderson99@gmail.com>
2013-02-13 23:40:17 +0000

    Added get episode by air date, and a bunch more unit tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,9 @@
 from pytvdb.impl.basic import HttpTVDBAdapter
 from pytvdb.model.exceptions import TVDBException
+from pytvdb.model.episode import Episode
 from nose.tools import assert_raises
+from datetime import date
 import os
-
 _ENVVAR_NAME='TVDBAPPLICATIONID' 
 
 def test_noid():
@@ -15,12 +16,50 @@ def test_noid():
         os.environ[_ENVVAR_NAME] = appid
     assert 'application_id is None and TVDBAPPLICATIONID is not set' == str(cm.exception)
 
-
-def test_fetch_and_parse():
+def test_search():
     adapter = HttpTVDBAdapter()
+    assert len(adapter.search(r"I don't exist, honest"))==0
+    results = adapter.search('90210')
+    print results
+    assert len(results)==3
+
+
+def test_get_and_parse():
+    adapter = HttpTVDBAdapter()
+    with assert_raises(TVDBException) as cm:
+        tv_show = adapter.get_show(r"I don't exist, honest")
+    assert "No series found for \"I don't exist, honest\"" == str(cm.exception)
+    
+    with assert_raises(TVDBException) as cm:
+        tv_show = adapter.get_show(r"90210")
+    assert "Multiple series found for \"90210\"" == str(cm.exception)
+    
     tv_show = adapter.get_show('Unit One')
     assert isinstance(tv_show.actors,list)
     assert tv_show.id == 78859
     assert tv_show.seasons[1].cover_art!=None
     assert tv_show.seasons[1].cover_art.banner_path==u'seasons/78859-2.jpg'
 
+
+def test_episode_by_airdate():
+    adapter = HttpTVDBAdapter()
+    with assert_raises(TVDBException) as cm:
+        episode = adapter.get_episode(r"I don't exist, honest",date(2000,10,01))
+    assert "No series found for \"I don't exist, honest\"" == str(cm.exception)
+     
+    with assert_raises(TVDBException) as cm:
+        episode = adapter.get_episode(r"90210",date(2000,10,10))
+    assert "Multiple series found for \"90210\"" == str(cm.exception)
+    
+    with assert_raises(TVDBException) as cm:
+        episode = adapter.get_episode(-1,-1)
+    assert "air_date must be a datetime.date" == str(cm.exception)
+    
+    with assert_raises(TVDBException) as cm:
+        episode = adapter.get_episode(-1,date(2000,10,01))
+    assert "No episode found" == str(cm.exception)
+    
+    episode = adapter.get_episode('Unit One',date(2000,10,01))
+    assert isinstance(episode,Episode)
+    print episode.__dict__
+    assert episode.episode_name == u"Assistance Report A-15/99"
