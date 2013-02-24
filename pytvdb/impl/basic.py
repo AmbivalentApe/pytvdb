b54728d72f085ac367d607fdfc725280673f8092
charver <philipanderson99@gmail.com>
2013-02-24 20:28:12 +0000

    Change to kwargs on __init__

--- pytvdb/impl/basic.py
+++ pytvdb/impl/basic.py
@@ -18,7 +18,10 @@ class HttpTVDBAdapter(TVDBShowProvider):
     
     '''
 
-    def __init__(self,application_id=None,base_url='http://www.thetvdb.com/api/',):
+    def __init__(self,**kwargs):
+        base_url='http://www.thetvdb.com/api/'
+
+        
         '''
         Constructor. 
 
@@ -27,12 +30,12 @@ class HttpTVDBAdapter(TVDBShowProvider):
         
         '''
         self.logger = logging.getLogger('httptvdbadapter')
-        if application_id is None:
+        if 'application_id' not in kwargs:
             if 'TVDBAPPLICATIONID' not in os.environ:
                 raise TVDBException(message='application_id is None and TVDBAPPLICATIONID is not set')
             self.application_id = os.environ['TVDBAPPLICATIONID']
         else:
-            self.application_id = application_id
+            self.application_id = kwargs['application_id']
         self.base_url = base_url
         
     def get_show(self,name,language='en'):
